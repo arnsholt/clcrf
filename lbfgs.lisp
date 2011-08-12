1cc687bb3f6b8617c7c596f08d442c3270ae1766
Arne Skjærholt <arnsholt@gmail.com>
2011-08-12 20:14:41 +0200

    Add basic vector inner and outer product, matrix transpose functions.

--- lbfgs.lisp
+++ lbfgs.lisp
@@ -5,3 +5,23 @@
 
 (defun line-search ()
   )
+
+; XXX: Will not complain if given vectors of different lengths.
+(defun inner-product (a b)
+  (loop for x across a
+        for y across b
+        summing (* x y)))
+
+(defun outer-product (a b)
+  (make-array (list (length a) (length b))
+              :initial-contents (map 'list (lambda (x) (map 'list (lambda (y) (* x y)) b)) a)))
+
+(defun transpose (m)
+  (loop with rows = (array-dimension m 0)
+        with columns = (array-dimension m 1)
+        with transpose = (make-array (list columns rows))
+        for row below rows
+        do (loop
+             for column below columns
+             do (setf (aref transpose column row) (aref m row column)))
+        finally (return transpose)))
