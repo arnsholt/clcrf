fb0289244b0e37494d88b3a0d19248256b2b1537
Arne Skjærholt <arnsholt@gmail.com>
2011-08-14 03:14:39 +0200

    Matrix product function.

--- lbfgs.lisp
+++ lbfgs.lisp
@@ -25,3 +25,24 @@
              for column below columns
              do (setf (aref transpose column row) (aref m row column)))
         finally (return transpose)))
+
+; XXX: Will not complain if the matrices are incompatible.
+(defun prod-m-m (a b)
+  (loop with rows    = (array-dimension a 0) ; No. of rows in result
+        with columns = (array-dimension b 1) ; No. of cols in result
+        with inner   = (array-dimension a 1) ; Dimension of "inner" edges of the matrices
+        with product = (make-array (list rows columns))
+        for row below rows
+        do (loop
+             for column below columns
+             for value = (loop
+                           for i below inner
+                           sum (* (aref a row i) (aref b i column)))
+             do (setf (aref product row column) value))
+        finally (return product)))
+
+(defun unit (dimen)
+  (loop with m = (make-array (list dimen dimen))
+        for i below dimen
+        do (setf (aref m i i) 1.0)
+        finally (return m)))
