f8e67290f3e5938a137dae8177e00f87a63160ac
Arne Skjærholt <arnsholt@gmail.com>
2011-08-17 18:36:37 +0200

    Revamped find-direction(), less wrong. Started on line-search().

--- lbfgs.lisp
+++ lbfgs.lisp
@@ -9,27 +9,38 @@
         for k from 0
         for (y g) = (multiple-value-list (funcall gradient x))
         for d = (find-direction k H0 x-history g g-history history)
-        for alpha = (line-search) ; TODO
+        for alpha = (line-search gradient d x y g)
         ))
 
 (defun find-direction (k H0 x-history g g-history history)
-  (loop with alpha = (make-array history)
-        with incr  = (if (<= k history) 0 (- k history))
-        with bound = (if (<= k history) k history)
-        for q = (loop
-                  for i from (1- bound) downto 0
-                  for j   = (+ i incr)
-                  for q   = g then (sub-v-v q (prod-s-v alpha (cref g-history j)))
-                  for rho = (/ 1 (inner-product (cref g-history j) (cref x-history j)))
-                  for a   = (* rho (inner-product (cref x-history j) q))
-                  do (setf (aref alpha i) a)
-                  finally (return q))
-        for i below history
-        for j    = (+ i incr)
-        for r    = (prod-m-v H0 q) then (add-m-m r (outer-product (cref g-history j) (sub-v-v (aref alpha i) beta)))
-        for rho  = (/ 1 (inner-product (cref g-history j) (cref x-history j)))
-        for beta = (* rho (inner-product (cref g-history j) r))
-            ))
+  (let* ((incr  (if (<= k history) 0 (- k history)))
+         (bound (if (<= k history) k history))
+         (q (make-array (1+ bound)))
+         (alpha (make-array bound)))
+    (loop initially (setf (aref q bound) g)
+          for i from (1- bound) downto 0
+          for j   = (+ i incr)
+          for rho = (/ 1 (inner-product (cref g-history j) (cref x-history j)))
+          for a   = (* rho (inner-product (cref x-history j) q))
+          do (setf (aref alpha i) a
+                   (aref q     i) (sub-v-v (aref q (1+ i)) (prod-s-v a (cref g-history j)))))
+    (loop with r = (make-array (1+ bound))
+          initially (setf (aref r 0) (prod-m-v H0 (aref q 0)))
+          for i below bound
+          for j    = (+ i incr)
+          for rho  = (/ 1 (inner-product (cref g-history j) (cref x-history j)))
+          for beta = (* rho (inner-product (cref g-history j) q))
+          do (setf (aref r (1+ i)) (add-m-m (aref r i) (outer-product (cref x-history j)
+                                                                      (sub-v-v (aref alpha i) beta))))
+          finally (return (aref r bound)))))
+
+(defun line-search (gradient direction x y g)
+  (loop with g-times-d = (inner-product g direction)
+        for alpha = 1 then (* 0.5 alpha)
+        for (y-prime g-prime) = (multiple-value-list (funcall gradient (add-v-v x (prod-s-v alpha direction))))
+        while (not (and (<= y-prime (+ y (* 1e-4 alpha g-times-d)))
+                        (>= (inner-product g-prime direction) (* 0.9 g-times-d))))
+        finally (return alpha)))
 
 ; XXX: Will not complain if given vectors of different lengths.
 (defun inner-product (a b)
@@ -59,6 +70,10 @@
              for j below (array-dimension a 1)
              do (setf (aref sum i j) (+ (aref a i j) (aref b i j))))
         finally (return sum)))
+
+; XXX: Will not complain if given vectors of different lengths.
+(defun add-v-v (a b)
+  (map 'vector (lambda (x y) (+ x y)) a b))
 
 ; XXX: Will not complain if given vectors of different lengths.
 (defun sub-v-v (a b)
